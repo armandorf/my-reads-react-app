065992da4aed604e49d92eea3d74e8ff0ea661eb
Pedro Flores <pflores@apttus.com>
2017-08-06 22:41:35 -0700

    feat: Add references to propTypes ‘books’ and function ‘onEditBook’

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
 
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onEditBook: PropTypes.func.isRequired
+  }
 }
 
 export default ListBooks
