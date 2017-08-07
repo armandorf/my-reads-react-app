aa7b4b5e42fc291b1acdc88420d1802177f2c767
Pedro Flores <pflores@apttus.com>
2017-08-06 22:22:11 -0700

    feat: Add files/components ‘ListBooks’ and ‘SearchBooks’

---
+++ src/ListBooks.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+class ListBooks extends Component {
+
+}
+
+export default ListBooks
