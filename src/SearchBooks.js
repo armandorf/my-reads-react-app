cd19986c275a7922b97ca0ec5a4dac6f6334c7d6
Pedro Flores <pflores@apttus.com>
2017-08-06 23:00:59 -0700

    feat: Implement the search functionality to retrieve books from the BooksApp component (top to bottom) Also implement the rendering of the list of retrieved books while allowing them to be edited

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -34,6 +34,48 @@ class SearchBooks extends Component {
         return match ? match : book
       })
     }
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {matchingBooks && (matchingBooks.map(book => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select value={book.shelf} onChange={(event) => onEditBook(book, event.target.value)}>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">
+                    {book.authors && book.authors.join(', ')}
+                  </div>
+                </div>
+              </li>
+            )))}
+          </ol>
+        </div>
+      </div>
+    )
   }
 }
 
