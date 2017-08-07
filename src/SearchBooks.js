920950d03286d0985af186733420d0137da1e2cc
Pedro Flores <pflores@apttus.com>
2017-08-06 22:55:32 -0700

    feat: Add propTypes ‘books’, ‘searchResult’, ‘onEditBook’, and ‘onUpdateQuery’ Also add state with property ‘query’ And finally implement component method updateQuery

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -4,6 +4,21 @@ import PropTypes from 'prop-types'
 
 class SearchBooks extends Component {
 
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    searchResult: PropTypes.array.isRequired,
+    onEditBook: PropTypes.func.isRequired,
+    onUpdateQuery: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query.trim()})
+    this.props.onUpdateQuery(query)
+  }
 }
 
 export default SearchBooks
