605aa8446c5a759070ea4470c98a1826b28b7056
Pedro Flores <pflores@apttus.com>
2017-08-06 22:53:02 -0700

    feat: Replace static html with dynamic JavaScript to render the books in the main page, the lister page Also add the ability to edit books’ shelf

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -35,6 +35,105 @@ class ListBooks extends Component {
     })
 
     return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {/* CURRENTLY READING SECTION */}
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {currentlyReadingBooks.map(book => book.shelf === CURRENTLY_READING_BOOKS_STR && (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onEditBook(book, event.target.value)}>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">
+                          {book.authors && book.authors.join(', ')}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            {/* WANT TO READ SECTION */}
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {wantToReadBooks.map(book => book.shelf === WANT_TO_READ_BOOKS_STR && (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onEditBook(book, event.target.value)}>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">
+                          {book.authors.join(', ')}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            {/* READ SECTION */}
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {readBooks.map(book => book.shelf === READ_BOOKS_STR && (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onEditBook(book, event.target.value)}>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">
+                          {book.authors.join(', ')}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            }
+          </div>
+        </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
