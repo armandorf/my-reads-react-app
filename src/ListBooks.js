26788a9a3d7a8371854161891c29c8a7461b2995
Pedro Flores <pflores@apttus.com>
2017-08-06 22:46:31 -0700

    feat: Implement the render method which contains a reference to the props and defines several constants for sorting out books by their reading status in 3 different arrays

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,6 +8,32 @@ class ListBooks extends Component {
     books: PropTypes.array.isRequired,
     onEditBook: PropTypes.func.isRequired
   }
+
+  render() {
+    const {books, onEditBook} = this.props
+    const CURRENTLY_READING_BOOKS_STR = 'currentlyReading'
+    const WANT_TO_READ_BOOKS_STR = 'wantToRead'
+    const READ_BOOKS_STR = 'read'
+    const currentlyReadingBooks = []
+    const wantToReadBooks = []
+    const readBooks = []
+
+    books.forEach((book) => {
+      switch(book.shelf) {
+        case CURRENTLY_READING_BOOKS_STR:
+          currentlyReadingBooks.push(book)
+          break
+        case WANT_TO_READ_BOOKS_STR:
+          wantToReadBooks.push(book)
+          break
+        case READ_BOOKS_STR:
+          readBooks.push(book)
+          break
+        default:
+          break
+      }
+    })
+  }
 }
 
 export default ListBooks
