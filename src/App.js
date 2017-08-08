07cc900597d4fb7cefeefe5ba6e4cc3f3ab9f08b
Pedro Flores <pflores@apttus.com>
2017-08-07 22:22:59 -0700

    refactor: Replace the use of string literals and numbers for constants

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import * as BooksAPI from './BooksAPI';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import './App.css';
+import {
+  MAX_RESULTS,
+  MAIN_PAGE_STR,
+  SEARCH_PAGE_STR
+} from './constants';
 
 class BooksApp extends React.Component {
   state = {
     books: [],
     searchResult: []
-  }
+  };
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
@@ -35,12 +40,10 @@ class BooksApp extends React.Component {
       })
     });
     BooksAPI.update(book, shelf);
-  };
-
-  static MAX_RESULTS = 50;
+  }
 
   searchBooks = (query) => {
-    BooksAPI.search(query.trim(), this.MAX_RESULTS).then((books) => {
+    BooksAPI.search(query.trim(), MAX_RESULTS).then((books) => {
       if (books && Array.isArray(books)) {
         books.sort(sortBy('title'));
         this.setState({ searchResult: books });
@@ -53,13 +56,13 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => (
+        <Route exact path={MAIN_PAGE_STR} render={() => (
           <ListBooks
             books={this.state.books}
             onEditBook={this.editBook}
           />)}
         />
-        <Route path="/search" render={({ history }) => (
+        <Route path={SEARCH_PAGE_STR} render={({ history }) => (
           <SearchBooks
             books={this.state.books}
             searchResult={this.state.searchResult}
