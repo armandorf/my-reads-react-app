e3c24ba27d10cf70330a7799243f93509e57d508
Pedro Flores <pflores@apttus.com>
2017-08-07 22:17:21 -0700

    feat: Add constants file

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+// book lister constants
+export const CURRENTLY_READING_BOOKS_VAL = 'currentlyReading';
+export const WANT_TO_READ_BOOKS_VAL = 'wantToRead';
+export const READ_BOOKS_VAL = 'read';
+export const CURRENTLY_READING_BOOKS_LABEL = "Currently Reading";
+export const WANT_TO_READ_BOOKS_LABEL = "Want to Read";
+export const READ_BOOKS_LABEL = "Read";
+
+// search functionality constants
+export const MAX_RESULTS = 50;
+
+// paths
+export const MAIN_PAGE_STR = '/';
+export const SEARCH_PAGE_STR = '/search';
